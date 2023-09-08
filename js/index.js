25a1bb2eb33546e395fc59dec294496e2fa13f90
shamim044 <shamim04433@gmail.com>
2023-09-09 03:44:14 +0600

    update bannersection

---
+++ js/index.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'text-color': '#121421',
+                'theme-color': '#FF2359',
+                'card-1': '#FEC52D',
+                'card-2': '#1FBCFF',
+                'card-3': '#FF8502',
+                'banner-bottom': '#121212cc',
+            },
+            backgroundImage: {
+                'hero-bg2': "linear-gradient(rgba(000, 000, 000, 0.6), rgba(000, 000, 000, 0.7)), url('./images/banner2.png')",
+                'hero-bg3': "linear-gradient(rgba(000, 000, 000, 0.6), rgba(000, 000, 000, 0.7)), url('./images/banner3.png')",
+                'hero-bg4': "linear-gradient(rgba(000, 000, 000, 0.6), rgba(000, 000, 000, 0.7)), url('./images/banner4.png')",
+            },
+        }
+    }
+};
